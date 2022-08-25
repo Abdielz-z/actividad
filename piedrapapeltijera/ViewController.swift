453ed3dcd3ad99b80fa1e93cbc6c9a5af4e50648
Abdiel Alejandro Ramírez Barrón <a01236062@chi-1115-21.svcs.itesm.mx>
2022-08-24 18:46:23 -0600

    last commmit was wrong changing to new file

--- piedrapapeltijera/ViewController.swift
+++ piedrapapeltijera/ViewController.swift
@@ -22,13 +22,55 @@ class ViewController: UIViewController {
 
     @IBAction func rock(_ sender: UIButton) {
         ganador.text = ""
-        
+        if let texto1 = num1.text{
+            if !texto1.isEmpty{
+                if let texto2 = num2.text{
+                    if !texto2.isEmpty{
+                        let iniciar = Logica()
+                        let fin = iniciar.Jugada(juego: Juego(valor: 1, compu: 0, p1: Int(texto1) ?? 0, p2: Int(texto2) ?? 0))
+                        ganador.text = fin.texto
+                        cpu.image = UIImage(named: fin.imagen)
+                        num1.text = "\(fin.p1)"
+                        num2.text = "\(fin.p2)"
+                    }
+                }
+            }
+        }
+         
     }
     @IBAction func paper(_ sender: UIButton) {
-    
+        ganador.text = ""
+        if let texto1 = num1.text{
+            if !texto1.isEmpty{
+                if let texto2 = num2.text{
+                    if !texto2.isEmpty{
+                        let iniciar = Logica()
+                        let fin = iniciar.Jugada(juego: Juego(valor: 2, compu: 0, p1: Int(texto1) ?? 0, p2: Int(texto2) ?? 0))
+                        ganador.text = fin.texto
+                        cpu.image = UIImage(named: fin.imagen)
+                        num1.text = "\(fin.p1)"
+                        num2.text = "\(fin.p2)"
+                    }
+                }
+            }
+        }
     }
     @IBAction func sissors(_ sender: UIButton) {
-        
+        ganador.text = ""
+        if let texto1 = num1.text{
+            if !texto1.isEmpty{
+                if let texto2 = num2.text{
+                    if !texto2.isEmpty{
+                        let iniciar = Logica()
+                        let fin = iniciar.Jugada(juego: Juego(valor: 3, compu: 0, p1: Int(texto1) ?? 0, p2: Int(texto2) ?? 0))
+                        ganador.text = fin.texto
+                        cpu.image = UIImage(named: fin.imagen)
+                        num1.text = "\(fin.p1)"
+                        num2.text = "\(fin.p2)"
+                    }
+                }
+            }
+        }
     }
     
 }
